55af177c6f8d12e5ecddaadc91c3a4d57db5e6b5
Bryan <bryanang1998@gmail.com>
2020-04-05 20:30:17 +1200

    removed keys from IService add method

--- Bangershare Backend/Interfaces/IService.cs
+++ Bangershare Backend/Interfaces/IService.cs
@@ -11,7 +11,7 @@ namespace Bangershare_Backend.Interfaces
     {
         Task<ICollection<TEntity>> GetAll();
         Task<TEntity> Get(params object[] keys);
-        Task<TResponse> Add(TEntity entity, params object[] keys);
+        Task<TResponse> Add(TEntity entity);
         Task<TResponse> Update(TEntity entity, params object[] keys);
         Task<TResponse> Delete(TEntity entity, params object[] keys);
     }

a8996a1018e886ae76149c32e7ab6eec8d076542
Bryan <bryanang1998@gmail.com>
2020-04-07 15:22:38 +1200

    Created DTO classes for songs, users and playlists

---
+++ Bangershare Backend/Dtos/SongDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bangershare_Backend.Dtos
+{
+    public class SongDto
+    {
+    }
+}

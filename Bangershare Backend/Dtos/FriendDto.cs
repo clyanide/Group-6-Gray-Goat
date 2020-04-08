e757f4e7c0ad0c9d237f4d4f5735597a0b51adc8
Bryan <bryanang1998@gmail.com>
2020-04-08 12:30:15 +1200

    Added friend and playlistsong dtos

---
+++ Bangershare Backend/Dtos/FriendDto.cs
@@ -0,0 +1,16 @@
+﻿using Bangershare_Backend.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bangershare_Backend.Dtos
+{
+    public class FriendDto
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public FriendType FriendType { get; set; }
+        public ICollection<PlaylistDto> Playlists { get; set; }
+    }
+}

99c0338735749ca8702f497afa11efa81b57f410
Bryan <bryanang1998@gmail.com>
2020-04-09 16:25:04 +1200

    Created config for tokens and signing

---
+++ Bangershare Backend/Security/Tokens/TokenOptions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bangershare_Backend.Security.Tokens
+{
+    public class TokenOptions
+    {
+        public string Audience { get; set; }
+        public string Issuer { get; set; }
+        public long AccessTokenExpiration { get; set; }
+        public long RefreshTokenExpiration { get; set; }
+    }
+}

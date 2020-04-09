4085c911a4bdb029c79d06acb0e6c8613718edfe
Bryan <bryanang1998@gmail.com>
2020-04-09 16:10:03 +1200

    Created access and refresh token classes

---
+++ Bangershare Backend/Models/Security/JsonWebToken.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bangershare_Backend.Models.Security
+{
+    public abstract class JsonWebToken
+    {
+        public string Token { get; set; }
+        public long Expiration { get; set; }
+
+        public JsonWebToken(string token, long expiration)
+        {
+            if (string.IsNullOrWhiteSpace(token))
+                throw new ArgumentException("Invalid token.");
+
+            if (expiration <= 0)
+                throw new ArgumentException("Invalid expiration.");
+
+            Token = token;
+            Expiration = expiration;
+        }
+
+        public bool IsExpired() => DateTime.UtcNow.Ticks > Expiration;
+    }
+}

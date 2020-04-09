2454cd5cabc4c0a18c1b34788040a63f143b6320
Bryan <bryanang1998@gmail.com>
2020-04-09 15:41:28 +1200

    Created a password hasher

---
+++ Bangershare Backend/Interfaces/IPasswordHasher.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bangershare_Backend.Interfaces
+{
+    public interface IPasswordHasher
+    {
+        string HashPassword(string password);
+        bool PasswordMatches(string providedPassword, string passwordHash);
+    }
+}

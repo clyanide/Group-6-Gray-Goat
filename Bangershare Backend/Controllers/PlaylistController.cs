eb8334c68ae9769ac7f1f589f97e399b2f7e4cf3
Bryan <bryanang1998@gmail.com>
2020-04-10 19:56:41 +1200

    Created playlist controller with create playlist method

---
+++ Bangershare Backend/Controllers/PlaylistController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Bangershare_Backend.Services;
+using Microsoft.AspNetCore.Authorization;
+using Bangershare_Backend.Dtos;
+using Bangershare_Backend.Models;
+using Bangershare_Backend.Helpers;
+
+namespace Bangershare_Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlaylistController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly PlaylistService _playlistService;
+        public PlaylistController(IMapper mapper, PlaylistService playlistService)
+        {
+            _mapper = mapper;
+            _playlistService = playlistService;
+        }
+
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> CreatePlaylist([FromBody] PlaylistDto playlistDto)
+        {
+            int userId = ClaimHelper.FindNameIdentifier(HttpContext.User.Claims);
+
+            var playlist = _mapper.Map<PlaylistDto, Playlist>(playlistDto);
+
+            var response = await _playlistService.CreatePlaylist(userId, playlist);
+
+            if (!response.Success)
+            {
+                return BadRequest(response.Message);
+            }
+
+            playlistDto = _mapper.Map<Playlist, PlaylistDto>(response.Resource);
+
+            return Ok(playlistDto);
+        }
+    }
+}

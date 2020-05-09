9082bee5ed75f45a1d2e97ae31da7c74ee962e7c
Bryan <bryanang1998@gmail.com>
2020-05-09 20:42:22 +1200

    Parsed  spotify json

--- Bangershare Backend/Services/SpotifyAPIService.cs
+++ Bangershare Backend/Services/SpotifyAPIService.cs
@@ -1,9 +1,10 @@
-﻿using AutoMapper.Configuration;
-using Bangershare_Backend.Models;
+﻿using Bangershare_Backend.Models;
+using Bangershare_Backend.Models.Security;
 using Bangershare_Backend.Services.Communications;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
@@ -15,41 +16,89 @@ namespace Bangershare_Backend.Services
     {
         private string trackURL = "https://api.spotify.com/v1/tracks/";
         private string authURL = "https://accounts.spotify.com/api/token";
-        private HttpClient _client;
+        private static HttpClient _client;
 
         private readonly SpotifyConfig _spotifyConfig;
 
         public SpotifyAPIService(SpotifyConfig spotifyConfig) 
         {
-            _spotifyConfig = spotifyConfig;
             _client = new HttpClient();
-        }
-
-        public async Task<BaseResponse<Song>> getSpotifySongInformation()
-        {
-            await postAuth();
-            throw new NotImplementedException();
-        }
-
-        private async Task postAuth()
-        {
             _client.DefaultRequestHeaders.Clear();
             _client.DefaultRequestHeaders.Accept.Add(
                 new MediaTypeWithQualityHeaderValue("application/json"));
 
+            _spotifyConfig = spotifyConfig;
+        }
+
+        public async Task<BaseResponse<Song>> getSpotifySongInformation(string spotifySongId)
+        {
+            var response = await postAuth();
+
+            if (!response.Success)
+            {
+                return new BaseResponse<Song>(response.Message);
+            }
+
+            SpotifyToken spotifyToken = response.Resource;
+
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(spotifyToken.token_type, spotifyToken.access_token);
+
+            var result = await _client.GetAsync(trackURL + spotifySongId);
+
+            if (result.IsSuccessStatusCode)
+            {
+                var jsonString = await result.Content.ReadAsStringAsync();
+                var obj = JsonConvert.DeserializeObject<SpotifySongModel>(jsonString);
+
+                throw new NotImplementedException();
+            }
+            else
+            {
+                return new BaseResponse<Song>(message: string.Format("{0} ({1})", (int)result.StatusCode, result.ReasonPhrase));
+            }
+        }
+
+        private async Task<BaseResponse<SpotifyToken>> postAuth()
+        {
             //Prepare Request Body
             List<KeyValuePair<string, string>> requestData = new List<KeyValuePair<string, string>>();
             requestData.Add(new KeyValuePair<string, string>("grant_type", "client_credentials"));
 
             FormUrlEncodedContent requestBody = new FormUrlEncodedContent(requestData);
 
-            var credentials = String.Format("{0}:{1}", _spotifyConfig.ClientID, _spotifyConfig.Secret);
+            string credentials = string.Format("{0}:{1}", _spotifyConfig.ClientID, _spotifyConfig.Secret);
 
             _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(Encoding.UTF8.GetBytes(credentials)));
 
             var result = await _client.PostAsync(authURL, requestBody);
-            var respone = await result.Content.ReadAsStringAsync();
-            var token = Newtonsoft.Json.JsonConvert.DeserializeObject<Bangershare_Backend.Models.Security.AccessToken>(respone);
+            if (result.IsSuccessStatusCode)
+            {
+                var jsonString = await result.Content.ReadAsStringAsync();
+
+                return new BaseResponse<SpotifyToken>(JsonConvert.DeserializeObject<SpotifyToken>(jsonString));
+            }
+            else
+            {
+                return new BaseResponse<SpotifyToken>(message: string.Format("{0} ({1})", (int)result.StatusCode, result.ReasonPhrase));
+            }
         }
+    }
+
+    internal class SpotifyToken
+    {
+        public string access_token { get; set; }
+        public string token_type { get; set; }
+        public long expires_in { get; set; }
+    }
+
+    internal class SpotifyArtistModel
+    {
+        public string name { get; set; }
+    }
+
+    internal class SpotifySongModel
+    {
+        public SpotifyArtistModel[] artists { get; set; }
+        public string name { get; set; }
     }
 }

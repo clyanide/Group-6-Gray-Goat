860b845720478c81b4881b03a074bab289fa33de
Bryan <bryanang1998@gmail.com>
2020-05-11 13:25:54 +1200

    Created friend service

---
+++ Bangershare Backend/Services/FriendService.cs
@@ -0,0 +1,25 @@
+﻿using Bangershare_Backend.Interfaces;
+using Bangershare_Backend.Models;
+using Bangershare_Backend.Repositories;
+using Bangershare_Backend.Services.Communications;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bangershare_Backend.Services
+{
+    public class FriendService : BaseService<BangerShareContext, Friend, FriendRepository, BaseResponse<Friend>, IUnitOfWork>
+    {
+        private readonly FriendRepository _friendRepository;
+        private readonly UserService _userService;
+
+        public FriendService(FriendRepository friendRepository, UserService userService, IUnitOfWork unitOfWork) : base(friendRepository, unitOfWork)
+        {
+            _friendRepository = friendRepository;
+            _userService = userService;
+        }
+
+
+    }
+}

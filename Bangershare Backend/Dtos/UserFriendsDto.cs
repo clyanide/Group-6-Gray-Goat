2f4ed3353cbac3a17d0a91a696776b3b19c99405
Bryan <bryanang1998@gmail.com>
2020-05-11 20:21:40 +1200

    Created userfriend and dto

---
+++ Bangershare Backend/Dtos/UserFriendsDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bangershare_Backend.Dtos
+{
+    public class UserFriendsDto
+    {
+        public ICollection<FriendDto> AcceptedFriends { get; set; }
+        public ICollection<FriendDto> PendingFriends { get; set; }
+    }
+}

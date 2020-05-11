e4a202e1b5ffc203378f9bd75872f6c75a132a23
Bryan <bryanang1998@gmail.com>
2020-05-11 13:25:48 +1200

    Added friend repo

---
+++ Bangershare Backend/Repositories/FriendRepository.cs
@@ -0,0 +1,13 @@
+﻿using Bangershare_Backend.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bangershare_Backend.Repositories
+{
+    public class FriendRepository : BaseRepository<Friend, BangerShareContext>
+    {
+        public FriendRepository(BangerShareContext context) : base(context) { }
+    }
+}

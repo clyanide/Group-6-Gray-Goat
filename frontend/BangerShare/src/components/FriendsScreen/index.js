ffa2029fc2fef586b84d271b0a80421155e63f6f
Clayton Lan <clayton.dev21@gmail.com>
2020-05-15 16:12:05 +1200

    Implemented Friend's list body

--- frontend/BangerShare/src/components/FriendsScreen/index.js
+++ frontend/BangerShare/src/components/FriendsScreen/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Container } from "native-base";
 import FriendsHeader from "./FriendsHeader";
+import ViewFriends from "./ViewFriends";
 
 const FriendsScreen = () => (
   <Container>
     <FriendsHeader />
+    <ViewFriends />
   </Container>
 );
 

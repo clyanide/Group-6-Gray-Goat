8a28dc8858d22b40a182a7134025600905add0f2
Lint Action <lint-action@samuelmeuli.com>
2020-05-15 01:00:13 +0000

    Fix code style issues with Prettier

--- frontend/BangerShare/src/components/HomeScreen/Color.js
+++ frontend/BangerShare/src/components/HomeScreen/Color.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React from "react";
 import { Text, Container, Button } from "native-base";
 
 const Color = (props) => {
-    const { getNewColor, color } = props
-    if (color) {
-        return (<Container>
-            <Text>{color}</Text>
-            <Button onPress={() => getNewColor()} />
-        </Container>)
-    }
+  const { getNewColor, color } = props;
+  if (color) {
+    return (
+      <Container>
+        <Text>{color}</Text>
+        <Button onPress={() => getNewColor()} />
+      </Container>
+    );
+  }
 
   return null;
 };
 
-export default Color
+export default Color;
--- frontend/BangerShare/src/containers/Color.js
+++ frontend/BangerShare/src/containers/Color.js
@@ -1,13 +1,15 @@
 import { getColor } from "../actions/Color";
-import { connect } from "react-redux"
-import Color from "../components/HomeScreen/Color"
+import { connect } from "react-redux";
+import Color from "../components/HomeScreen/Color";
 
 const mapStateToProps = (state) => {
-    return { color: state.ColorReducer.color }
-}
+  return { color: state.ColorReducer.color };
+};
 
 const mapDispatchToProps = (dispatch) => ({
-    getNewColor: () => { dispatch(getColor()) }
-})
+  getNewColor: () => {
+    dispatch(getColor());
+  },
+});
 
-export default connect(mapStateToProps, mapDispatchToProps)(Color)
+export default connect(mapStateToProps, mapDispatchToProps)(Color);

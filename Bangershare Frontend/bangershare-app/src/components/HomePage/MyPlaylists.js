3e957742ab9a0fc568d68cd9b52038b600cf2684
Thomas <thomaszhu.nz@gmail.com>
2020-05-17 20:09:22 +1200

    Refactored playlists

--- Bangershare Frontend/bangershare-app/src/components/HomePage/MyPlaylists.js
+++ Bangershare Frontend/bangershare-app/src/components/HomePage/MyPlaylists.js
@@ -1,24 +1,35 @@
 import React from "react";
-import Carousel from "react-multi-carousel";
-import "react-multi-carousel/lib/styles.css";
+import createPlaylist from "./Playlist"
 
-const responsive = {
-  desktop: {
-    breakpoint: { max: 3000, min: 1024 },
-    items: 5,
-    slidesToSlide: 3, // optional, default to 1.
+const playlists = [
+  {
+    name: "test"
   },
-  tablet: {
-    breakpoint: { max: 1024, min: 464 },
-    items: 2,
-    slidesToSlide: 2, // optional, default to 1.
+  {
+    name: "hi"
   },
-  mobile: {
-    breakpoint: { max: 464, min: 0 },
-    items: 1,
-    slidesToSlide: 1, // optional, default to 1.
+  {
+    name: "lol"
   },
-};
+  {
+    name: "test"
+  },
+  {
+    name: "hi"
+  },
+  {
+    name: "lol"
+  },
+  {
+    name: "test"
+  },
+  {
+    name: "hi"
+  },
+  {
+    name: "lol"
+  },
+]
 
 const MyPlaylists = () => {
   return (
@@ -26,31 +37,7 @@ const MyPlaylists = () => {
       <div>
         <p>My Playlists</p>
       </div>
-      <Carousel
-        swipeable={true}
-        draggable={true}
-        responsive={responsive}
-        infinite={true}
-        keyBoardControl={true}
-        customTransition="all .5"
-        transitionDuration={500}
-        containerClass="carousel-container"
-        dotListClass="custom-dot-list-style"
-        itemClass="carousel-item-padding-40-px"
-      >
-        <div>Item 1</div>
-        <div>Item 2</div>
-        <div>Item 3</div>
-        <div>Item 4</div>
-        <div>Item 1</div>
-        <div>Item 2</div>
-        <div>Item 3</div>
-        <div>Item 4</div>
-        <div>Item 1</div>
-        <div>Item 2</div>
-        <div>Item 3</div>
-        <div>Item 4</div>
-      </Carousel>
+      {createPlaylist(playlists)}
     </div>
   );
 };
--- Bangershare Frontend/bangershare-app/src/components/HomePage/RecentPlaylists.js
+++ Bangershare Frontend/bangershare-app/src/components/HomePage/RecentPlaylists.js
@@ -1,24 +1,20 @@
 import React from "react";
-import Carousel from "react-multi-carousel";
-import "react-multi-carousel/lib/styles.css";
+import createPlaylist from "./Playlist"
 
-const responsive = {
-  desktop: {
-    breakpoint: { max: 3000, min: 1024 },
-    items: 5,
-    slidesToSlide: 3, // optional, default to 1.
+const playlists = [
+  {
+    name: "sjhdakjdasdsa"
   },
-  tablet: {
-    breakpoint: { max: 1024, min: 464 },
-    items: 2,
-    slidesToSlide: 2, // optional, default to 1.
+  {
+    name: "dasdsadsdsd"
   },
-  mobile: {
-    breakpoint: { max: 464, min: 0 },
-    items: 1,
-    slidesToSlide: 1, // optional, default to 1.
+  {
+    name: "Bangers"
   },
-};
+  {
+    name: "stuff"
+  },
+]
 
 const RecentPlaylists = () => {
   return (
@@ -26,31 +22,7 @@ const RecentPlaylists = () => {
       <div>
         <p>Recent Playlists</p>
       </div>
-      <Carousel
-        swipeable={true}
-        draggable={true}
-        responsive={responsive}
-        infinite={true}
-        keyBoardControl={true}
-        customTransition="all .5"
-        transitionDuration={500}
-        containerClass="carousel-container"
-        dotListClass="custom-dot-list-style"
-        itemClass="carousel-item-padding-40-px"
-      >
-        <div>Item 1</div>
-        <div>Item 2</div>
-        <div>Item 3</div>
-        <div>Item 4</div>
-        <div>Item 1</div>
-        <div>Item 2</div>
-        <div>Item 3</div>
-        <div>Item 4</div>
-        <div>Item 1</div>
-        <div>Item 2</div>
-        <div>Item 3</div>
-        <div>Item 4</div>
-      </Carousel>
+      {createPlaylist(playlists)}
     </div>
   );
 };

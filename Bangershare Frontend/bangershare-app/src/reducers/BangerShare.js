da262467648e3201effc58608cab75078688c1fb
Thomas <thomaszhu.nz@gmail.com>
2020-05-17 20:40:12 +1200

    refactored bangershare reducer

--- Bangershare Frontend/bangershare-app/src/reducers/BangerShare.js
+++ Bangershare Frontend/bangershare-app/src/reducers/BangerShare.js
@@ -1,45 +1,30 @@
-import { friendActionType } from "./../actions/Friends";
-import { userActionType } from "./../actions/User";
-
 const initialState = {
-  fetching: false,
-  error: "",
+    fetching: false,
+    error: "",
 };
 
 const setLoading = (state, boolean) => {
-  return { ...state, fetching: boolean };
+    return { ...state, fetching: boolean };
 };
 
 const setError = (state, action) => {
-  return {
-    ...state,
-    fetching: true,
-    error: action.error,
-  };
+    return {
+        ...state,
+        fetching: false,
+        error: action.error,
+    };
 };
+
 const bangerShareReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case friendActionType.GET_FRIENDS:
-      return setLoading(state, true);
-    case friendActionType.GET_FRIENDS_SUCCESS:
-      return setLoading(state, false);
-    case friendActionType.GET_FRIENDS_FAIL:
-      return setError(state, action);
-    case userActionType.REGISTER_USER:
-      return setLoading(state, true);
-    case userActionType.REGISTER_USER_SUCCESS:
-      return setLoading(state, false);
-    case userActionType.REGISTER_USER_FAIL:
-      return setError(state, action);
-    case userActionType.LOGIN_USER:
-      return setLoading(state, true);
-    case userActionType.LOGIN_USER_SUCCESS:
-      return setLoading(state, false);
-    case userActionType.LOGIN_USER_FAIL:
-      return setLoading(state, false);
-    default:
-      return { ...state };
-  }
+    if (action.fetching === true) {
+        return setLoading(state, true)
+    } else if (action.error) {
+        return setError(state, action)
+    } else if (action.fetching === false) {
+        return setLoading(state, false)
+    }
+
+    return { ...state }
 };
 
 export default bangerShareReducer;

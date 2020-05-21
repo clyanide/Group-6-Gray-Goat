bf01fd53d13391d622b01f3e99d8ae6808c65729
Bryan <bryanang1998@gmail.com>
2020-05-21 15:44:20 +1200

    Added userlike migration and updated db

--- Bangershare Backend/Models/UserLike.cs
+++ Bangershare Backend/Models/UserLike.cs
@@ -9,7 +9,7 @@ namespace Bangershare_Backend.Models
     {
         public User User { get; set; }
         public Song Song { get; set; }
-        public int UserId { get; set; }
-        public int SongId { get; set; }
+        public virtual int UserId { get; set; }
+        public virtual int SongId { get; set; }
     }
 }

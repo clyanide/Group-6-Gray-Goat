8dfa1c557b17acdd10b2c893f4eb28866ff0fc65
Thomas <thomaszhu.nz@gmail.com>
2020-05-22 16:40:47 +1200

    Moved theme to app.js

--- Bangershare Frontend/bangershare-app/src/App.js
+++ Bangershare Frontend/bangershare-app/src/App.js
@@ -3,11 +3,26 @@ import { Provider } from "react-redux";
 import { store } from "./store";
 import "./App.css";
 import BangerShareApp from "./BangerShareApp";
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
+import { purple } from '@material-ui/core/colors';
+
+const BangerShareTheme = createMuiTheme({
+  palette: {
+    type: 'dark',
+  },
+  primary: {
+    // Purple and green play nicely together.
+    main: purple[500],
+  },
+});
 
 function App() {
   return (
     <Provider store={store}>
-      <BangerShareApp />
+      <ThemeProvider theme={BangerShareTheme}>
+        <BangerShareApp />
+      </ThemeProvider>
     </Provider>
   );
 }

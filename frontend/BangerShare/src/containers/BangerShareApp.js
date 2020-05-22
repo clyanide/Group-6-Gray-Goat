8fffed575ca80db63736f9b4b5e9d8df2b7cf098
Clayton Lan <clayton.dev21@gmail.com>
2020-05-23 00:57:30 +1200

    Removed legacy react native project

--- frontend/BangerShare/src/containers/BangerShareApp.js
+++
@@ -1,8 +0,0 @@
-import { connect } from "react-redux";
-import BangerShareApp from "../BangerShareApp";
-
-const mapStateToProps = (state) => {
-  return { accessToken: state.UserReducer.accessToken };
-};
-
-export default connect(mapStateToProps)(BangerShareApp);
--- frontend/BangerShare/src/epics/index.js
+++
@@ -1,7 +0,0 @@
-import { combineEpics } from "redux-observable";
-import { getColor } from "./Color";
-import { registerUser, loginUser } from "./User";
-
-const rootEpic = combineEpics(getColor, registerUser, loginUser);
-
-export default rootEpic;

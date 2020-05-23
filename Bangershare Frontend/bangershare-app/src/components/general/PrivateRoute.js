324eaac5878ea26707cf59df816cdddaae5f3496
Bryan <bryanang1998@gmail.com>
2020-05-24 01:45:22 +1200

    Added private router which redirects if not authed

---
+++ Bangershare Frontend/bangershare-app/src/components/general/PrivateRoute.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+    const bangerShareToken = localStorage.getItem("token")
+    const spotifyToken = localStorage.getItem("spotifyToken")
+    return (
+        <Route
+            {...rest}
+            render={(props) => bangerShareToken !== null && spotifyToken !== null
+                ? <Component {...props} />
+                : <Redirect to={{ pathname: bangerShareToken === null ? "/login" : "/spotifyauth", state: { from: props.location } }} />
+            }
+        />
+    );
+}
+
+export default PrivateRoute;

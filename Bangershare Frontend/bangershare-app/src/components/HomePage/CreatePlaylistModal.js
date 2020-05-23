f70abe19283912b29ab3ed82fd46a22cdf33c072
Clayton Lan <clayton.dev21@gmail.com>
2020-05-23 15:48:18 +1200

    Add playlist/song modals now using unstyled MUI componenets

--- Bangershare Frontend/bangershare-app/src/components/HomePage/CreatePlaylistModal.js
+++ Bangershare Frontend/bangershare-app/src/components/HomePage/CreatePlaylistModal.js
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
-import { Button, Input, Modal } from "semantic-ui-react";
+import Modal from "@material-ui/core/Modal";
+import TextField from "@material-ui/core/TextField";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
+import Button from "@material-ui/core/Button";
 
 const CreatePlaylistModal = (props) => {
   const { open, handleModal, createPlaylist } = props;
@@ -15,24 +18,55 @@ const CreatePlaylistModal = (props) => {
     handleModal(false);
   };
 
-  return (
-    <Modal open={open} onClose={() => handleModal(false)}>
-      <Modal.Header>Give your playlist a name.</Modal.Header>
-      <Modal.Content>
-        <Input
-          placeholder={"Please enter a name"}
-          onChange={(e) => handleNameOnChange(e)}
-        />
-      </Modal.Content>
-      <Modal.Actions>
-        <Button.Group>
+  const body = (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+        position: "absolute",
+        width: "30vw",
+        height: "20vh",
+      }}
+    >
+      <div style={{ flex: 1, paddingTop: "1vh" }}>
+        <h2>Give your playlist a name</h2>
+      </div>
+      <div style={{ flex: 1 }}>
+        <form>
+          <TextField
+            label="Please enter a name"
+            variant="outlined"
+            onChange={(e) => handleNameOnChange(e)}
+          />
+        </form>
+      </div>
+      <div style={{ flex: 1, paddingTop: "2vh" }}>
+        <ButtonGroup>
           <Button onClick={() => handleModal(false)}>Cancel</Button>
-          <Button positive onClick={() => handleOnCreate()}>
-            Create
-          </Button>
-        </Button.Group>
-      </Modal.Actions>
-    </Modal>
+          <Button onClick={() => handleOnCreate()}>Create</Button>
+        </ButtonGroup>
+      </div>
+    </div>
+  );
+  return (
+    <div>
+      <Modal
+        open={open}
+        onClose={() => handleModal(false)}
+        style={{
+          backgroundColor: "gray",
+          width: "30vw",
+          height: "20vh",
+          position: "absolute",
+          left: "35vw",
+          top: "40vh",
+        }}
+      >
+        {body}
+      </Modal>
+    </div>
   );
 };
 
--- Bangershare Frontend/bangershare-app/src/components/PlaylistPage/CreateSongModal.js
+++ Bangershare Frontend/bangershare-app/src/components/PlaylistPage/CreateSongModal.js
@@ -1,5 +1,9 @@
 import React, { useState } from "react";
-import { Button, Header, Form, Modal } from "semantic-ui-react";
+// import { Button, Header, Form, Modal } from "semantic-ui-react";
+import Modal from "@material-ui/core/Modal";
+import TextField from "@material-ui/core/TextField";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
+import Button from "@material-ui/core/Button";
 
 const CreateSongModal = (props) => {
   const { open, handleModal, postSong } = props;
@@ -29,86 +33,191 @@ const CreateSongModal = (props) => {
     handleModal(false);
   };
 
-  return (
-    <Modal open={open} onClose={() => handleModal(false)}>
-      <Header
-        icon="music"
-        content={
-          <>
-            Add a song
-            <Button.Group>
-              <Button
-                disabled={!spotify}
-                onClick={() => {
-                  toggleSpotify();
-                  setSongDetail("songType", 2);
-                }}
-              >
-                Youtube
-              </Button>
-              <Button
-                disabled={spotify}
-                onClick={() => {
-                  toggleSpotify();
-                  setSongDetail("songType", 0);
-                }}
-              >
-                Spotify
-              </Button>
-            </Button.Group>
-          </>
-        }
-      />
-      <Modal.Content>
-        <Form>
-          {!spotify ? (
-            <>
-              <Form.Field>
-                <label>Song name</label>
-                <input
-                  placeholder={"Enter a name of a song"}
+  const body = (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+        position: "absolute",
+        width: "30vw",
+        height: "20vh",
+      }}
+    >
+      <div>
+        <h2>Add a song</h2>
+      </div>
+      <div>
+        <ButtonGroup>
+          <Button
+            disabled={!spotify}
+            onClick={() => {
+              toggleSpotify();
+              setSongDetail("songType", 2);
+            }}
+          >
+            Youtube
+          </Button>
+          <Button
+            disabled={spotify}
+            onClick={() => {
+              toggleSpotify();
+              setSongDetail("songType", 0);
+            }}
+          >
+            Spotify
+          </Button>
+        </ButtonGroup>
+      </div>
+      <div>
+        {!spotify ? (
+          <div>
+            <div>
+              <form>
+                <TextField
+                  label="Song Name"
+                  variant="outlined"
                   onChange={(e) => setSongDetail("name", e.target.value)}
                 />
-              </Form.Field>
-              <Form.Field>
-                <label>Artist Name</label>
-                <input
-                  placeholder={"Enter the artist of the song"}
+              </form>
+            </div>
+            <div>
+              <form>
+                <TextField
+                  label="Artist Name"
+                  variant="outlined"
                   onChange={(e) => setSongDetail("artist", e.target.value)}
                 />
-              </Form.Field>
-              <Form.Field>
-                <label>{"URL of Youtube Song"}</label>
-                <input
-                  placeholder={
-                    'e.g. "https://www.youtube.com/watch?v=S0twBO8l3pI"'
-                  }
+              </form>
+            </div>
+            <div>
+              <form>
+                <TextField
+                  label="Youtube URL (e.g. https://www.youtube.com/watch?v=S0twBO8l3pI)"
+                  variant="outlined"
                   onChange={(e) => setSongDetail("link", e.target.value)}
                 />
-              </Form.Field>
-            </>
-          ) : (
-            <Form.Field>
-              <label>{"Spotify Song ID"}</label>
-              <input
-                placeholder={
-                  'e.g. "https://open.spotify.com/track/3c7peg169veVaJRzlbCaKw"'
-                }
+              </form>
+            </div>
+          </div>
+        ) : (
+          <div>
+            <form>
+              <TextField
+                label="Spotify Song ID (e.g. https://open.spotify.com/track/3c7peg169veVaJRzlbCaKw)"
+                variant="outlined"
                 onChange={(e) => setSongDetail("link", e.target.value)}
               />
-            </Form.Field>
-          )}
-        </Form>
-      </Modal.Content>
-      <Modal.Actions>
-        <Button.Group>
+            </form>
+          </div>
+        )}
+      </div>
+      <div>
+        <ButtonGroup>
           <Button onClick={() => handleModal(false)}>Cancel</Button>
-          <Button positive onClick={() => createSong()}>
-            Create
-          </Button>
-        </Button.Group>
-      </Modal.Actions>
-    </Modal>
+          <Button onClick={() => createSong()}>Create</Button>
+        </ButtonGroup>
+      </div>
+    </div>
+  );
+  return (
+    <div>
+      <Modal
+        open={open}
+        onClose={() => handleModal(false)}
+        style={{
+          backgroundColor: "gray",
+          width: "30vw",
+          height: "20vh",
+          position: "absolute",
+          left: "35vw",
+          top: "40vh",
+        }}
+      >
+        {body}
+      </Modal>
+    </div>
+
+    // <Modal open={open} onClose={() => handleModal(false)}>
+    //   <Header
+    //     icon="music"
+    //     content={
+    //       <>
+    //         Add a song
+    //         <Button.Group>
+    //           <Button
+    //             disabled={!spotify}
+    //             onClick={() => {
+    //               toggleSpotify();
+    //               setSongDetail("songType", 2);
+    //             }}
+    //           >
+    //             Youtube
+    //           </Button>
+    //           <Button
+    //             disabled={spotify}
+    //             onClick={() => {
+    //               toggleSpotify();
+    //               setSongDetail("songType", 0);
+    //             }}
+    //           >
+    //             Spotify
+    //           </Button>
+    //         </Button.Group>
+    //       </>
+    //     }
+    //   />
+    //   <Modal.Content>
+    //     <Form>
+    //       {!spotify ? (
+    //         <>
+    //           <Form.Field>
+    //             <label>Song name</label>
+    //             <input
+    //               placeholder={"Enter a name of a song"}
+    //               onChange={(e) => setSongDetail("name", e.target.value)}
+    //             />
+    //           </Form.Field>
+    //           <Form.Field>
+    //             <label>Artist Name</label>
+    //             <input
+    //               placeholder={"Enter the artist of the song"}
+    //               onChange={(e) => setSongDetail("artist", e.target.value)}
+    //             />
+    //           </Form.Field>
+    //           <Form.Field>
+    //             <label>{"URL of Youtube Song"}</label>
+    //             <input
+    //               placeholder={
+    //                 'e.g. "https://www.youtube.com/watch?v=S0twBO8l3pI"'
+    //               }
+    //               onChange={(e) => setSongDetail("link", e.target.value)}
+    //             />
+    //           </Form.Field>
+    //         </>
+    //       ) : (
+    //         <Form.Field>
+    //           <label>{"Spotify Song ID"}</label>
+    //           <input
+    //             placeholder={
+    //               'e.g. "https://open.spotify.com/track/3c7peg169veVaJRzlbCaKw"'
+    //             }
+    //             onChange={(e) => setSongDetail("link", e.target.value)}
+    //           />
+    //         </Form.Field>
+    //       )}
+    //     </Form>
+    //   </Modal.Content>
+    //   <Modal.Actions>
+    //     <Button.Group>
+    //       <Button onClick={() => handleModal(false)}>Cancel</Button>
+    //       <Button positive onClick={() => createSong()}>
+    //         Create
+    //       </Button>
+    //     </Button.Group>
+    //   </Modal.Actions>
+    // </Modal>
   );
 };
 
